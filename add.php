7c2869bb9598fab9ac143972f456a3b632a0f187
Amish087 <coolamish014@gmail.com>
2025-10-30 12:11:43 +0530

    Update add.php

--- add.php
+++ add.php
@@ -3,10 +3,10 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $name = $_POST['name'];
-  $email = $_POST['email'];
+  $email = $_POST['attendance'];
   $course = $_POST['course'];
 
-  $conn->query("INSERT INTO students (name, email, course) VALUES ('$name', '$email', '$course')");
+  $conn->query("INSERT INTO students (name, attendance, course) VALUES ('$name', '$attendance', '$course')");
   header("Location: index.php");
 }
 ?>
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <h2>Add Student</h2>
   <form method="POST">
     Name: <input type="text" name="name" required><br><br>
-    Email: <input type="email" name="email" required><br><br>
+    Email: <input type="attendance" name="attendance" required><br><br>
     Course: <input type="text" name="course" required><br><br>
     <button type="submit">Save</button>
   </form>
